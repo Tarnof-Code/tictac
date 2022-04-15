fad4d4a0c36340a0cf642811ec077c77788ee3cb
Tarnof <tarnof@hotmail.com>
2022-04-15 10:10:02 +0200

    Heroku3

--- Tic Tac/routes/users.js
+++ Tic Tac/routes/users.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var userModel = require("../models/users");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const BDD_LOGIN = process.env.BDD_LOGIN;
+const BDD_PASSWORD = process.env.BDD_PASSWORD;
+
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
